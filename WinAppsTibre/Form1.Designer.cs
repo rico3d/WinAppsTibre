38083a2552f89c1658300633f0d8e0e0a51a0b0b
Ricardo Ronaldo Pinto <rico3d2001@gmail.com>
2018-05-09 14:30:24 -0300

    Ajuste inical para Timbre prototipo

--- WinAppsTibre/Form1.Designer.cs
+++ WinAppsTibre/Form1.Designer.cs
@@ -28,12 +28,80 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtBoxMDepois = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 72);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(318, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "TROCAR";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtBoxMDepois
+            // 
+            this.txtBoxMDepois.Location = new System.Drawing.Point(116, 6);
+            this.txtBoxMDepois.Name = "txtBoxMDepois";
+            this.txtBoxMDepois.Size = new System.Drawing.Size(214, 20);
+            this.txtBoxMDepois.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Prefixo da MARCA:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Prefixo do ITEM:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(116, 32);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(214, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(350, 109);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtBoxMDepois);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "TROCA PREFIXOS TIBRE";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtBoxMDepois;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
     }
 }
 
--- WinAppsTibre/Form1.cs
+++ WinAppsTibre/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Tekla.Structures.Model;
 
 namespace WinAppsTibre
 {
@@ -15,6 +16,89 @@ namespace WinAppsTibre
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Model model = new Model();
+
+            if (!model.GetConnectionStatus())
+            {
+                MessageBox.Show(string.Format("Connection failed!", new object[0]));
+                Application.Exit();
+            }
+
+            var vigas = model.GetModelObjectSelector().GetAllObjectsWithType(ModelObject.ModelObjectEnum.BEAM);
+
+
+
+            if (vigas.GetSize() > 0)
+            {
+                while (vigas.MoveNext())
+                {
+                    
+                    var aPart = vigas.Current as Part;
+
+                    string partMarca = string.Empty;
+                    aPart.GetReportProperty("ASSEMBLY_POS", ref partMarca);
+
+                    string partPos = string.Empty;
+                    aPart.GetReportProperty("PART_POS", ref partPos);
+
+                    
+                    NumberingSeries numberingSeriesItem = 
+                        !partPos.Contains('?') ? NumberingSeriesItem(aPart, partPos) : null;
+
+                    NumberingSeries numberingSeriesMarca = 
+                        !partMarca.Contains('?') ? NumberingSeriesMarca(partMarca) : null;
+
+                    if (numberingSeriesItem != null)
+                    {
+                        aPart.PartNumber = numberingSeriesItem;
+                    }
+
+                    if (numberingSeriesMarca != null)
+                    {
+                        aPart.AssemblyNumber = numberingSeriesMarca;
+                    }
+                   
+                   
+                    aPart.Modify();
+
+
+
+                }
+
+
+
+            }
+        }
+
+        private NumberingSeries NumberingSeriesItem(Part aPart, string partPos)
+        {
+            NumberingSeries numberingSeriesItem;
+            int compItemAntes = aPart.PartNumber.Prefix.Length;
+            string prefixoItem = partPos.Substring(0, compItemAntes);
+            int serialItem = int.Parse(partPos.Substring(compItemAntes));
+            prefixoItem = textBox2.Text;
+            numberingSeriesItem = new NumberingSeries(prefixoItem, serialItem);
+            return numberingSeriesItem;
+        }
+
+        private NumberingSeries NumberingSeriesMarca(string partMarca)
+        {
+            NumberingSeries numberingSeriesMarca;
+            string prefixoMarca = partMarca.Split('/')[0];
+            int serialMarca = int.Parse(partMarca.Split('/')[1]);
+            prefixoMarca = txtBoxMDepois.Text;
+            numberingSeriesMarca = new NumberingSeries(prefixoMarca, serialMarca);
+            return numberingSeriesMarca;
+        }
+
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
